6016caf89727c4c49f6a05760fe870ed5baf5f10
MeleshcheniaK <61804405+MeleshcheniaK@users.noreply.github.com>
2021-05-04 20:11:30 +0300

    Update globals.py

--- globals.py
+++ globals.py
@@ -1,4 +1,8 @@
+# Appid для сайта с прогнозом погоды
 APPID = "9293be28c948ca37fa0758f4027419fe"
+# Token для TelegramBot
 TOKEN = '1728312997:AAH8EXGILjzPNV3rwHex-8EfLY6hggncUdc'
+# Начальное состояние конечного автомата
 CITY_ENTER = 0
+# Ссылка на сайт с пресказаниями
 URL = 'https://8-gund.com/ru/'
