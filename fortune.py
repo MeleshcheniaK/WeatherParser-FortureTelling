e66a2237d133aae4a21757d0b20782f6982c19a2
MeleshcheniaK <61804405+MeleshcheniaK@users.noreply.github.com>
2021-05-04 20:12:56 +0300

    Update fortune.py

--- fortune.py
+++ fortune.py
@@ -3,16 +3,17 @@ import requests
 import time
 from selenium import webdriver
 
+# Подключение веб-браузера
 options = webdriver.ChromeOptions()
 options.add_argument("headless")
 driver = webdriver.Chrome(r'D:\Informatic\Python\Bot/chromedriver', chrome_options=options)
 
-
+# Обработка ссылки
 def get_html(url):
     r = requests.get(url)
     return r
 
-
+# Получение ответа от шара
 def get_answer():
     driver.get(globals.URL)
     time.sleep(1)
@@ -22,7 +23,7 @@ def get_answer():
     element = driver.find_element_by_class_name('ball-text').text
     return element
 
-
+# Подключение к сайту
 def parse():
     html = get_html(globals.URL)
     if html.status_code == 200:
