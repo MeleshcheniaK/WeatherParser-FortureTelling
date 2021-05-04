2dc1f7fd3c9b0c3b30fc2fe988b75c1dae2eb9fb
MeleshcheniaK <61804405+MeleshcheniaK@users.noreply.github.com>
2021-05-04 20:00:57 +0300

    Update fortune.py

--- fortune.py
+++ fortune.py
@@ -8,9 +8,10 @@ options.add_argument("headless")
 driver = webdriver.Chrome(r'D:\Informatic\Python\Bot/chromedriver', chrome_options=options)
 
 
-def get_html(url, params = None):
-    r = requests.get(url, params = params)
+def get_html(url):
+    r = requests.get(url)
     return r
+
 
 def get_answer():
     driver.get(globals.URL)
@@ -20,6 +21,7 @@ def get_answer():
     time.sleep(1)
     element = driver.find_element_by_class_name('ball-text').text
     return element
+
 
 def parse():
     html = get_html(globals.URL)
