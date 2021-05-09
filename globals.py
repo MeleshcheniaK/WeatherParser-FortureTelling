75d61b907e5b3d21447062cfafda3bcc94177c83
MeleshcheniaK <61804405+MeleshcheniaK@users.noreply.github.com>
2021-05-09 20:40:51 +0300

    Updating security

--- globals.py
+++ globals.py
@@ -1,7 +1,7 @@
 # Appid для сайта с прогнозом погоды
-APPID = "9293be28c948ca37fa0758f4027419fe"
+APPID = "*"
 # Token для TelegramBot
-TOKEN = '1728312997:AAH8EXGILjzPNV3rwHex-8EfLY6hggncUdc'
+TOKEN = '*'
 # Начальное состояние конечного автомата
 CITY_ENTER = 0
 # Ссылка на сайт с пресказаниями
