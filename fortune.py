1475e37358589c5c895939c62a71df86f8ada665
MeleshcheniaK <61804405+MeleshcheniaK@users.noreply.github.com>
2021-05-20 18:14:22 +0300

    Update fortune

--- fortune.py
+++ fortune.py
@@ -1,12 +1,16 @@
-import globals
+import os
 import time
+
 from selenium import webdriver
+
+import globals
 
 # Подключение веб-браузера
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 driver = webdriver.Chrome(f'{os.path.dirname(os.path.abspath(__file__))}/ChromeDriver/chromedriver',
                           chrome_options=options)
+
 
 # Получение ответа от шара
 def get_orb_answer():
@@ -18,10 +22,3 @@ def get_orb_answer():
     element = driver.find_element_by_class_name('ball-text').text
     return element
 
-# Подключение к сайту
-def parse():
-    html = get_html(globals.URL)
-    if html.status_code == 200:
-        get_answer(html.text)
-    else:
-        print('Error')
